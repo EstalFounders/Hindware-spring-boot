7c3f1ed592f7d79e6fc71167f917251d0d3c7f81
rentic-admin <raju26435@gmail.com>
2022-02-22 15:25:35 +0530

    Updated code for testing heroku

--- src/main/java/com/estal/Estal/rest/SignUp.java
+++ src/main/java/com/estal/Estal/rest/SignUp.java
@@ -4,10 +4,7 @@ package com.estal.Estal.rest;
 import com.estal.Estal.entity.UserEntity;
 import com.estal.Estal.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/signup")
@@ -20,5 +17,10 @@ public class SignUp {
     public String saveTheUser(@RequestBody UserEntity userEntity) {
         userService.saveTheUser(userEntity);
        return "User stored successfully";
+    }
+
+    @GetMapping("/test")
+    public String testing() {
+        return  "testing for heroku working";
     }
 }
