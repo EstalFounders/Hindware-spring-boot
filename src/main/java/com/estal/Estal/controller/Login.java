3a9602bf1fccfba23e0eaf53610092507c8925aa
EstalFounders <raju26435@gmail.com>
2022-05-15 19:30:12 +0530

    New hindware database added

--- src/main/java/com/estal/Estal/controller/Login.java
+++ src/main/java/com/estal/Estal/controller/Login.java
@@ -3,16 +3,11 @@ package com.estal.Estal.controller;
 import com.estal.Estal.Jwt.JwtUtil;
 import com.estal.Estal.Models.AuthenticationRequest;
 import com.estal.Estal.Models.AuthenticationResponse;
-import com.estal.Estal.dao.UserRepository;
 import com.estal.Estal.entity.SihEntity;
 import com.estal.Estal.service.UserLoginService;
-import net.bytebuddy.implementation.bytecode.Throw;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
@@ -37,7 +32,7 @@ public class Login {
              UserDetails userDetails = userLoginService.loadUserByUsername(authenticationRequest.getUsername());
              final String jwt = jwtUtil.generateToken(userDetails);
              if (ResponseEntity.ok(new AuthenticationResponse(jwt)) != null) {
-                 return this.getUserDetails(authenticationRequest);
+                 return this.getSihUserDetails(authenticationRequest);
              } else {
                  throw new Exception("Something went wrong in login, Maybe Wrong credentials") ;
              }
@@ -46,8 +41,8 @@ public class Login {
          }
     }
 
-    @GetMapping("/getUserDetails")
-    public SihEntity getUserDetails(AuthenticationRequest authenticationRequest) {
+    @GetMapping("/getSihUserDetails")
+    public SihEntity getSihUserDetails(AuthenticationRequest authenticationRequest) {
         try {
             return userLoginService.getUserDetails(authenticationRequest.getUsername());
         } catch (Exception e) {
